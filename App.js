53fbc83b860d46bcf2136ba866c256b7560c3711
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2023-12-21 17:20:52 +0000

    Basics of React Native like view, touchables etc.

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("Text clicked");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        TouchableWithoutFeedback
+      </Text>
+      {/* <Image source={require("./assets/icon.png")} /> */}
+      <TouchableWithoutFeedback onPress={() => console.log("Image tapped")}>
+        <Image
+          width={300}
+          height={200}
+          blurRadius={10}
+          fadeDuration={1000}
+          resizeMode="contain"
+          source={{ uri: "https://picsum.photos/200/300" }}
+        />
+      </TouchableWithoutFeedback>
+
+      <Text numberOfLines={1}>Touchable Opacity</Text>
+      <TouchableOpacity onPress={() => console.log("Image tapped")}>
+        <Image
+          width={300}
+          height={200}
+          blurRadius={10}
+          fadeDuration={1000}
+          resizeMode="contain"
+          source={{ uri: "https://picsum.photos/200/300" }}
+        />
+      </TouchableOpacity>
+      <Text numberOfLines={1}>TouchableHeighlight</Text>
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        <Image
+          width={300}
+          height={200}
+          blurRadius={10}
+          fadeDuration={1000}
+          resizeMode="contain"
+          source={{ uri: "https://picsum.photos/200/300" }}
+        />
+      </TouchableHighlight>
+      <Text numberOfLines={1}>TouchableNativFeedback (only Android)</Text>
+      <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
+        <View
+          style={{ width: 200, height: 70, backgroundColor: "pink" }}
+        ></View>
+      </TouchableNativeFeedback>
+      <StatusBar style="dark" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "dodgerblue",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+Tutorial by Mosh Hamedani on YT: https://www.youtube.com/watch?v=0-S5a0eXPoc
+
+Good explanation how to Debug in React Native
