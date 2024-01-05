c59ae0304bfd4243a59e41f2fdcb5ff23bcf274e
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2024-01-05 21:00:18 +0000

    custom button

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 // import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Platform, StatusBar, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Platform,
+  StatusBar,
+  Dimensions,
+} from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
   // return <WelcomeScreen />;
-  return <ViewImageScreen />;
+  // return <ViewImageScreen />;
+  <View>
+    <Text>Welcome</Text>
+  </View>;
 }
 
 const styles = StyleSheet.create({});
