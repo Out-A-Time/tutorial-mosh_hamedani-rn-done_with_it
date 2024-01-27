68b552285b11fa4f31d33e8f356435020f747f3d
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2024-01-27 19:23:44 +0000

    update of React Native to v0.73.2

---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,12 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MessagesScreen = () => {
+  return (
+<FlatList>
+  );
+};
+
+export default MessagesScreen;
+
+const styles = StyleSheet.create({});
